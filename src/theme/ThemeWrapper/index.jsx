ba3596abeb667d1b4e79be177e87411bf709a504
Dilnaz Zhumabaikyzy <dilnazzh200303@gmail.com>
2022-06-17 13:24:49 +0600

    dark-mode

--- src/theme/ThemeWrapper/index.jsx
+++ src/theme/ThemeWrapper/index.jsx
@@ -12,10 +12,25 @@ export default function ThemeContextWrapper(props) {
     switch (theme) {
       case themes.light:
         document.body.classList.add('white-content');
-        document.getElementById('footer').classList.add('white-content');
-        console.log(document.getElementById('footer').classList);
+        document.getElementById('footer').classList.remove('dark');
+        document.getElementById('footer').classList.add('light');
+        // console.log(document.getElementById('footer').classList);
+
+        for (let item of document.getElementsByClassName('favorite-button')) {
+        item.classList.add('light');
+        item.classList.remove('dark');
+        }
         break;
       case themes.dark:
+        document.body.classList.remove('white-content');
+        document.getElementById('footer').classList.add('dark');
+        document.getElementById('footer').classList.remove('light');
+        for (let item of document.getElementsByClassName('favorite-button')) {
+        item.classList.add('dark');
+        item.classList.remove('light');
+       }
+        // console.log(document.getElementById('footer').classList);
+        break;
       default:
         document.body.classList.remove('white-content');
         break;
